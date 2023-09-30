33e87c4e7d37dc17e8ddec5f5122e83519dec1a1
yk21ju@gmail.com <yk21ju@gmail.com>
2023-09-30 15:57:39 -0700

    test

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "../components/Header";
 import Skip from "../components/Skip";
+import Intro from "../components/Intro";
+import Skill from "../components/Skill";
+import Site from "../components/Site";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
       <Skip />
       <Header />
+      <main id="main" role="main">
+        <Intro />
+        <Skill />
+        <Site />
+        <Contact />
+      </main>
+      <Footer />
     </>
   );
 };
