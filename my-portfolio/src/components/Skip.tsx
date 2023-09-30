2f2b832b77be301769d6d30d42696017ff6e6e04
yk21ju@gmail.com <yk21ju@gmail.com>
2023-09-30 16:56:44 -0700

    header

--- my-portfolio/src/components/Skip.tsx
+++ my-portfolio/src/components/Skip.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Skip = () => {
   return (
-    <div>
-      Skip
+    <div id="skip">
+      <a href="#header">Goto Header</a>
+      <a href="#intro">Goto Intro </a>
+      <a href="#skill">Goto Skill</a>
+      <a href="#site">Goto Site</a>
+      <a href="#port">Goto Portfolio</a>
+      <a href="#contact">Goto Contact</a>
+      <a href="#footer">Goto Footer</a>
     </div>
-  )
-}
+  );
+};
 
-export default Skip
+export default Skip;
