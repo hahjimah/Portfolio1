8f52521886af5f17705050b9eeeeb07c06e66fbd
yk21ju@gmail.com <yk21ju@gmail.com>
2023-12-11 21:23:33 -0800

    clean

--- my-portfolio/src/components/Experience.tsx
+++ my-portfolio/src/components/Experience.tsx
@@ -6,7 +6,7 @@ const Experience = () => {
     <section id="experience">
       <div className="experience__inner">
         <h2 className="experience__title">
-          Experiences<em>my experiences</em>
+          Experiences<em> my experiences</em>
         </h2>
         <div className="experience__desc">
           {experienceText.map((exp, key) => (
@@ -23,29 +23,3 @@ const Experience = () => {
 };
 
 export default Experience;
-
-// import React from "react";
-// import { skillText } from "../constants";
-
-// const Skill = () => {
-//   return (
-//     <section id="skill">
-//       <div className="slkll__inner">
-//         <h2 className="skill__title">
-//           challenge <em>My Inspirations</em>
-//         </h2>
-//         <div className="skill__desc">
-//           {skillText.map((skill, key) => (
-//             <div key={key}>
-//               <span>{key + 1}.</span>
-//               <h3>{skill.title}</h3>
-//               <p>{skill.desc}</p>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Skill;
